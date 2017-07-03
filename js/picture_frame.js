cac7a749156db857ba294e465ffd098112a4745a
Simon Berger <siku2@outlook.de>
2017-07-03 22:57:57 +0200

    picture frame pretty much implemented

--- js/picture_frame.js
+++ js/picture_frame.js
@@ -1,19 +1,67 @@
 function pictureFrameHandlePlayerInformation(player) {
 	"use strict";
+	
+	var title_element = document.getElementById("title");
+	var artist_element = document.getElementById("artist");
+	var cover_element = document.getElementById("cover");
+	var pause_indicator = document.getElementById("paused");
+	
 	if ([1, 2].indexOf(player.state) >= 0) { //is the player either paused or playing
 		var entry = player.entry;
 		document.getElementById("thumbnail").src = entry.thumbnail;
-		document.getElementById("paused").style.visibility = (player.state === 1) ? "hidden" : "visible";
+		pause_indicator.style.visibility = (player.state === 1) ? "hidden" : "visible";
 
 		switch (entry.type) {
 			case "spotify_entry":
-				document.getElementById("artist").innerHTML = entry.artist;
-				document.getElementById("title").innerHTML = entry.name;
-				document.getElementById("cover").src = entry.cover_url;
+				artist_element.style.display = "";
+				title_element.style.display = "";
+				cover_element.style.display = "";
+				
+				artist_element.innerHTML = entry.artist;
+				title_element.innerHTML = entry.name;
+				cover_element.src = entry.cover_url;
+				break;
+			case "radio_entry":
+				artist_element.style.display = "";
+				title_element.style.display = "";
+				cover_element.style.display = "";
+				
+				artist_element.innerHTML = entry.artist;
+				title_element.innerHTML = entry.title;
+				cover_element.src = entry.cover;
+				break;
+			case "radio_entry":
+				artist_element.style.display = "none";
+				title_element.style.display = "";
+				cover_element.style.display = "";
+				
+				title_element.innerHTML = entry.name;
+				cover_element.src = entry.cover;
+				break;
+			case "timestamp_entry":
+				artist_element.style.display = "";
+				title_element.style.display = "";
+				cover_element.style.display = "none";
+				
+				artist_element.innerHTML = "From " + entry.title;
+				title_element.innerHTML = entry.sub_title;
+				break;
+			default:
+				artist_element.style.display = "none";
+				title_element.style.display = "";
+				cover_element.style.display = "none";
+				
+				title_element.innerHTML = entry.title;
 				break;
 		}
 	}
 	else {
 		//display "nothing playing" or something
+		artist_element.style.display = "none";
+		title_element.style.display = "";
+		cover_element.style.display = "none";
+		pause_indicator.style.display = "none";
+
+		title_element.innerHTML = "Nothing playing";
 	}
 }
