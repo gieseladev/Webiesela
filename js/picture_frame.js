3b1453979c907fe1a9382f020a76af366c5cdf02
Simon <simon.siku@yahoo.de>
2017-07-06 20:40:01 +0100

    sooo many animations

--- js/picture_frame.js
+++ js/picture_frame.js
@@ -1,50 +1,59 @@
+function switchCover(new_cover_url) {
+	"use strict";
+	var flip_container = document.getElementById("cover_flip_container");
+	var back_cover_element = document.getElementById("back_cover");
+	var cover_element = document.getElementById("cover");
+	back_cover_element.src = cover_element.src;
+	cover_element.src = new_cover_url;
+	flip_container.classList.add("flip");
+	var callfunction = function() {
+		flip_container.classList.remove("flip");
+	};
+	flip_container.addEventListener("webkitAnimationEnd", callfunction, false);
+	flip_container.addEventListener("animationend", callfunction, false);
+	flip_container.addEventListener("oanimationend", callfunction, false);
+}
+
 function pictureFrameHandlePlayerInformation(player) {
 	"use strict";
-	
 	var title_element = document.getElementById("title");
 	var artist_element = document.getElementById("artist");
 	var cover_element = document.getElementById("cover");
 	var pause_indicator = document.getElementById("paused");
-	
 	if ([1, 2].indexOf(player.state) >= 0) { //is the player either paused or playing
 		var entry = player.entry;
-		document.getElementById("thumbnail").src = entry.thumbnail ? entry.thumbnail : "";
+		transitionBackground(entry.thumbnail ? entry.thumbnail : "");
 		pause_indicator.style.visibility = (player.state === 1) ? "hidden" : "visible";
 		pause_indicator.className = "";
-
 		switch (entry.type) {
 			case "spotify_entry":
 				artist_element.style.display = "";
 				title_element.style.display = "";
 				cover_element.style.display = "";
-				
 				artist_element.innerHTML = entry.artist;
 				title_element.innerHTML = entry.name;
-				cover_element.src = entry.cover_url;
+				switchCover(entry.cover_url);
 				break;
 			case "radio_entry":
 				artist_element.style.display = "";
 				title_element.style.display = "";
 				cover_element.style.display = "";
-				
 				artist_element.innerHTML = entry.artist;
 				title_element.innerHTML = entry.title;
-				cover_element.src = entry.cover;
+				switchCover(entry.cover);
 				break;
 			case "radio_entry":
 				artist_element.style.display = "none";
 				title_element.style.display = "";
 				cover_element.style.display = "";
-				
 				title_element.innerHTML = entry.name;
-				cover_element.src = entry.cover;
+				switchCover(entry.cover);
 				break;
 			case "timestamp_entry":
 				artist_element.style.display = "";
 				title_element.style.display = "";
 				cover_element.style.display = "none";
 				pause_indicator.className = "absolute";
-				
 				artist_element.innerHTML = "From " + entry.title;
 				title_element.innerHTML = entry.sub_title;
 				break;
@@ -53,18 +62,16 @@ function pictureFrameHandlePlayerInformation(player) {
 				title_element.style.display = "";
 				cover_element.style.display = "none";
 				pause_indicator.className = "absolute";
-				
 				title_element.innerHTML = entry.title;
 				break;
 		}
-	}
-	else {
+	} else {
 		//display "nothing playing" or something
 		artist_element.style.display = "none";
 		title_element.style.display = "";
 		cover_element.style.display = "none";
 		pause_indicator.style.display = "none";
-
 		title_element.innerHTML = "Nothing playing";
+		transitionBackground("");
 	}
 }
--- js/utils.js
+++ js/utils.js
@@ -11,3 +11,19 @@ function selectText(containerid) {
 		window.getSelection().addRange(range);
 	}
 }
+
+function transitionBackground(new_background_url) {
+	"use strict";
+	var bg_parent = document.getElementById("bg");
+	var old_thumbnail = document.getElementById("thumbnail_old");
+	var thumbnail = document.getElementById("thumbnail");
+	old_thumbnail.src = thumbnail.src;
+	thumbnail.src = new_background_url;
+	bg_parent.classList.add("transition");
+	var callfunction = function() {
+		bg_parent.classList.remove("transition");
+	};
+	bg_parent.addEventListener("webkitAnimationEnd", callfunction, false);
+	bg_parent.addEventListener("animationend", callfunction, false);
+	bg_parent.addEventListener("oanimationend", callfunction, false);
+}
