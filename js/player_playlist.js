77fdaf2161d4858bb51dc5afb2493ecd65ee0832
Simon Berger <siku2@outlook.de>
2017-08-11 04:07:06 +0200

    some playlists progress

--- js/player_playlist.js
+++ js/player_playlist.js
@@ -1,10 +1,63 @@
 var playlists;
 
 
+function displayEntries(parentElement, entries) {
+  "use strict";
+
+  while (parentElement.firstChild) {
+    parentElement.removeChild(parentElement.firstChild);
+  }
+
+  var entry_template = document.getElementById("entry_template").cloneNode(true);
+
+  entry_template.removeAttribute("id");
+  entry_template.removeAttribute("style");
+
+  var index = 1;
+
+  for (var entry of entries) {
+    var entry_element = entry_template.cloneNode(true);
+
+    entry_element.getElementsByClassName("index")[0].innerHTML = index;
+    entry_element.getElementsByClassName("title")[0].innerHTML = entry.title;
+
+    if (entry.album && entry.artist) {
+      entry_element.getElementsByClassName("album")[0].innerHTML = entry.album;
+      entry_element.getElementsByClassName("artist")[0].innerHTML = entry.artist;
+    } else {
+      entry_element.getElementsByClassName("name")[0].removeChild(entry_element.getElementsByClassName("origin")[0]);
+    }
+
+    entry_element.getElementsByClassName("duration")[0].innerHTML = formatSeconds(entry.duration);
+
+    parentElement.appendChild(entry_element);
+    index++;
+  }
+}
+
 function showPlaylist(playlist_id) {
   "use strict";
 
-  console.log("Showing playlist", playlist_id);
+  var playlist = playlists.find(function(playlist) {
+    return playlist.id === playlist_id
+  });
+  console.log("Showing playlist", playlist);
+  document.getElementById("playlist_display").style.display = "none";
+
+  displayEntries(document.getElementById("playlist_entries"), playlist.entries);
+
+  document.getElementById("playlist_cover").onclick = function() {
+    loadPlaylist(playlist_id);
+  };
+
+	document.getElementById("focused_playlist_cover").style.backgroundImage = "url('" + playlist.cover + "')";
+	document.getElementById("playlist_title").innerHTML = playlist.name;
+	document.getElementById("playlist_author").innerHTML = "by " + playlist.author.display_name;
+	document.getElementById("playlist_description").innerHTML = playlist.description;
+	document.getElementById("playlist_entry_amount").innerHTML = playlist.entries.length + " songs";
+	document.getElementById("playlist_playtime").innerHTML = "wip";
+
+  document.getElementById("focused_display").style.display = "";
 }
 
 function loadPlaylist(playlist_id) {
@@ -26,20 +79,20 @@ function displayPlaylists() {
 
   playlist_template.removeAttribute("id");
   playlist_template.removeAttribute("style");
-	
-	var hoverClick = function(playlistId) {
-		return function(event) {
-			if (!event.target.classList.contains("play_button")) {
-      			showPlaylist(playlistId);
-			}
-    	};
-	};
-	
-	var playClick = function(playlistId) {
-		return function() {
-      		loadPlaylist(playlistId);
-    	};
-	};
+
+  var hoverClick = function(playlistId) {
+    return function(event) {
+      if (!event.target.classList.contains("play_button")) {
+        showPlaylist(playlistId);
+      }
+    };
+  };
+
+  var playClick = function(playlistId) {
+    return function() {
+      loadPlaylist(playlistId);
+    };
+  };
 
   for (var playlist of playlists) {
     var playlist_element = playlist_template.cloneNode(true);
@@ -49,7 +102,7 @@ function displayPlaylists() {
 
     playlist_element.getElementsByClassName("author")[0].innerHTML = "by " + playlist.author.display_name;
 
-    playlist_element.getElementsByClassName("hover_target")[0].addEventListener("click", hoverClick(playlist.id));
+    playlist_element.addEventListener("click", hoverClick(playlist.id));
     playlist_element.getElementsByClassName("play_button")[0].addEventListener("click", playClick(playlist.id));
 
     parentElement.appendChild(playlist_element);
@@ -57,8 +110,8 @@ function displayPlaylists() {
 }
 
 function receivePlaylist(answer) {
-	"use strict";
-	
-	playlists = answer.playlists;
-	displayPlaylists();
+  "use strict";
+
+  playlists = answer.playlists;
+  displayPlaylists();
 }
