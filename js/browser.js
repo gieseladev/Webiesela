7a0a78db9587f06ea51352cdfe63ce6720a7ab38
Simon Berger <siku2@outlook.de>
2017-11-06 01:14:55 +0100

    finished YoutubeSearcher and SpotifySearcher authorisation

--- js/browser.js
+++ js/browser.js
@@ -20,15 +20,7 @@ class Playlist {
 }
 
 class Searcher {
-  static search(query) {}
-
-  static getUrl(url) {}
-
-  static featured() {}
-}
-
-class YoutubeSearcher extends Searcher {
-  static rawSearch(query) {
+  static get(url, headers) {
     return new Promise(function(resolve, reject) {
       let request = new XMLHttpRequest();
 
@@ -38,20 +30,84 @@ class YoutubeSearcher extends Searcher {
 
       request.addEventListener("load", respHandler);
 
-      request.open("GET", "https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=20&q=" + encodeURI(query) + "&topicId=%2Fm%2F04rlf&type=video%2Cplaylist&key=" + config.googleApiKey);
+      request.open("GET", url);
+
+      if (headers) {
+        for (var i = 0; i < headers.length; i++) {
+          request.setRequestHeader(headers[i][0], headers[i][1]);
+        }
+      }
+
       request.send();
     });
   }
 
+  static post(url, headers) {
+    return new Promise(function(resolve, reject) {
+      let request = new XMLHttpRequest();
+
+      function respHandler() {
+        resolve(this.response);
+      }
+
+      request.addEventListener("load", respHandler);
+
+      request.open("POST", url);
+
+      if (headers) {
+        for (var i = 0; i < headers.length; i++) {
+          request.setRequestHeader(headers[i][0], headers[i][1]);
+        }
+      }
+
+      request.send();
+    });
+  }
+
+  static search(query) {}
+
+  static getUrl(url) {}
+
+  static featured() {}
+}
+
+class YoutubeSearcher extends Searcher {
+  static rawSearch(query) {
+    return super.get("https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=20&q=" + encodeURI(query) + "&topicId=%2Fm%2F04rlf&type=video%2Cplaylist&key=" + config.googleApiKey);
+  }
+
+  static rawFetch(url) {
+    return new Promise(function(resolve, reject) {
+      const re = /(?:watch|playlist)\?(v|list)=([A-Za-z0-9\-_]{8,})/g;
+      let res = re.exec(url);
+
+      if (res) {
+        let kind = res[1];
+        let id = res[2];
+
+        switch (kind) {
+          case "v":
+            Searcher.get("https://www.googleapis.com/youtube/v3/videos?part=snippet&id=" + id + "&key=" + config.googleApiKey).then(resolve);
+            break;
+          case "list":
+            Searcher.get("https://www.googleapis.com/youtube/v3/playlists?part=snippet%2CcontentDetails&id=" + id + "&key=" + config.googleApiKey).then(resolve);
+            break;
+        }
+      } else {
+        reject(Error("Can't parse this url!"));
+      }
+    });
+  }
+
   static itemBuilder(result) {
-    let kind = result.id.kind;
+    let kind = result.id.kind || result.kind;
 
     switch (kind) {
       case "youtube#video":
-        return new Entry(result.snippet.title, result.snippet.channelTitle, result.snippet.thumbnails.high.url, null, "https://www.youtube.com/watch?v=" + result.id.videoId);
+        return new Entry(result.snippet.title, result.snippet.channelTitle, result.snippet.thumbnails.high.url, null, "https://www.youtube.com/watch?v=" + (result.id.videoId || result.id));
         break;
       case "youtube#playlist":
-        return new Playlist(result.snippet.title, result.snippet.channelTitle, result.snippet.thumbnails.high.url, null, "https://www.youtube.com/playlist?list=" + result.id.playlistId);
+        return new Playlist(result.snippet.title, result.snippet.channelTitle, result.snippet.thumbnails.high.url, result.contentDetails.itemCount, "https://www.youtube.com/playlist?list=" + (result.id.playlistId || result.id));
         break;
     }
   }
@@ -70,36 +126,60 @@ class YoutubeSearcher extends Searcher {
     });
   }
 
-  static getUrl(url, cb) {
-    let result;
+  static getUrl(url) {
+    return new Promise(function(resolve, reject) {
+      YoutubeSearcher.rawFetch(url).then(JSON.parse).then(function(response) {
+        let result = YoutubeSearcher.itemBuilder(response.items[0]);
 
-    cb(result);
+        resolve(result);
+      });
+    });
   }
 
-  static featured(cb) {
-    let results;
+  static featured() {
+    return new Promise(function(resolve, reject) {
+      // Youtube's "music" channel
+      const videoChannel = "UC-9-kyTW8ZkZNDHQJ6FgpwQ";
+      let url = "https://www.googleapis.com/youtube/v3/playlists?part=snippet%2CcontentDetails&channelId=" + videoChannel + "&maxResults=20&key=" + config.googleApiKey;
 
-    cb(results);
+      Searcher.get(url).then(JSON.parse).then(function(response) {
+        let results = [];
+
+        for (var i = 0; i < response.items.length; i++) {
+          results.push(YoutubeSearcher.itemBuilder(response.items[i]));
+        }
+
+        resolve(results);
+      });
+    });
   }
 }
 
 class SpotifySearcher extends Searcher {
-  static search(query, cb) {
-    let results;
-
-    cb(results);
+  static get accessHeader() {
+    return new Promise(function(resolve, reject) {
+      if (SpotifySearcher.accessToken && SpotifySearcher.accessToken.expires_at > (Date.now() / 1000)) {
+        resolve(["Authorization", "Bearer " + SpotifySearcher.accessToken.access_token]);
+      } else {
+        console.log("[SpotifySearcher] getting new token");
+        Searcher.get("http://utils.giesela.org/tokens/spotify", ["Access-Control-Allow-Origin", true]).then(JSON.parse).then(function(accessToken) {
+          SpotifySearcher.accessToken = accessToken;
+          resolve(["Authorization", "Bearer " + SpotifySearcher.accessToken.access_token]);
+        });
+      }
+    });
   }
 
-  static getUrl(url, cb) {
-    let result;
+  static search(query) {}
 
-    cb(result);
-  }
+  static getUrl(url) {}
 
-  static featured(cb) {
-    let results;
-
-    cb(results);
+  static featured() {
+    return new Promise(function(resolve, reject) {
+      SpotifySearcher.accessHeader.then(function(header) {
+        Searcher.get("https://api.spotify.com/v1/browse/featured-playlists", [header]).then(resolve);
+      });
+    });
   }
 }
 
@@ -112,16 +192,16 @@ class Browser {
     this.searcher = newSearcher;
   }
 
-  search(query, cb) {
-    return this.searcher.search(query, cb);
+  search(query) {
+    return this.searcher.search(query);
   }
 
-  getUrl(url, cb) {
-    return this.searcher.getUrl(url, cb);
+  getUrl(url) {
+    return this.searcher.getUrl(url);
   }
 
-  featured(cb) {
-    return this.searcher.featured(cb);
+  featured() {
+    return this.searcher.featured();
   }
 }
 
